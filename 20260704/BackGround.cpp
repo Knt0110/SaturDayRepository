9775083666aa3bc1eae74df5c48555dd346f5dd3
MATAYOSHI RYO <158718894+ryo-cpu@users.noreply.github.com>
2026-07-11 09:43:03 +0900

    updata

---
+++ 20260704/BackGround.cpp
@@ -0,0 +1,18 @@
+#include "Background.h"
+#include "DxLib.h"
+
+//=====================================================
+// èâä˙âª
+//=====================================================
+void Background::Init()
+{
+	imageHandle = LoadGraph("img/Background.png");
+}
+
+//=====================================================
+// ï`âÊ
+//=====================================================
+void Background::Draw(float cameraX)
+{
+	DrawGraph(-(int)(cameraX * 0.5f),0,imageHandle,TRUE);
+}
